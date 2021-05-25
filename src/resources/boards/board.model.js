197559ed8607fe6e57f01e67ac8ac57900f1c9a3
Korshunov Egor <egor.dezreng@gmail.com>
2021-05-25 15:57:07 +0300

    feat: add JsDoc comments for board

--- src/resources/boards/board.model.js
+++ src/resources/boards/board.model.js
@@ -1,7 +1,12 @@
 const { v4: uuidv4 } = require('uuid');
 const Colum = require('./board.colum');
 
+/** Class representing a Board */
 class Board {
+	/**
+	 * Create a Board
+	 * @param {object} param0 
+	 */
   constructor({
     id = uuidv4(),
     title = 'BOARD',
@@ -12,6 +17,11 @@ class Board {
 		this.columns = columns !== null ? columns.map(col => new Colum(col)) : null;
   }
 
+	/**
+	 * Transforming a board for a response
+	 * @param {object} board 
+	 * @returns {object}
+	 */
   static toResponse(board) {
     const { id, title, columns } = board;
     return { id, title, columns };
--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,13 +1,38 @@
 const boardsRepo = require('./board.memory.repository');
 
-const getAll = () => boardsRepo.getAll();
+/**
+ * Return all boards in the database
+ * @returns {Promise<object>}
+ */
+const getAll = (idBoard) => boardsRepo.getAll(idBoard);
 
+/**
+ * Return board in the database
+ * @param {string} id 
+ * @returns {Promise<object>}
+ */
 const get = (id) => boardsRepo.get(id);
 
+/**
+ * Adding a board to the database
+ * @param {object} reqBody 
+ * @returns {Promise<object>}
+ */
 const add = (reqBody) => boardsRepo.add(reqBody);
 
+/**
+ * Updating a board in the database
+ * @param {string} id 
+ * @param {object} params 
+ * @returns {Promise<object>}
+ */
 const update = (id, params) => boardsRepo.update(id, params);
 
+/**
+ * Removing a board from the database
+ * @param {string} id 
+ * @returns {Promise<object>}
+ */
 const remove = (id) => boardsRepo.remove(id);
 
 module.exports = { getAll, get, add, update, remove };
