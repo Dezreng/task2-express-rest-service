5e12ed7711b054bce0c00f8a3c02f3c63f0a33ea
Korshunov Egor <egor.dezreng@gmail.com>
2021-06-06 21:41:26 +0300

    feat: add middleware error and new Class Error

--- src/error/errorHandler.ts
+++ src/error/errorHandler.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import logger from '../logger/moduleLogger';
 
 interface errHanler extends Error {
 	name: string;
@@ -7,7 +8,15 @@ interface errHanler extends Error {
 	status?: number;
 };
 
-const errorHandler = (err: errHanler, _req: Request, res: Response, _next: NextFunction) => {
+const errorHandler = (err: errHanler, req: Request, res: Response, _next: NextFunction) => {
+	logger.error(`method: ${req.method}, URL: ${decodeURI(
+        req.url
+      )}, query parameters: ${JSON.stringify(
+        req.query
+      )}, request body: ${JSON.stringify(
+        req.body
+      )}, ${err.status}, message: ${err.message}`);
+
 	res.status(err.status || 500).send({
       error: {
         status: err.status || 500,
