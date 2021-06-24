8e4cc9ff593ef72235830e1ecda3ae948b64836c
Korshunov Egor <egor.dezreng@gmail.com>
2021-06-24 12:58:42 +0300

    feat: add new user admin admin for task

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
+import bcrypt from 'bcrypt';
 import config  from './common/config';
 import app from './app';
 import logger from './logger/moduleLogger';
 import configDB from './common/ormconfig';
+import User from './entity/user.model';
 
 
 createConnection(configDB).then( async connection => {
 	if ( connection.isConnected ) {
 		console.log("DataBase is connected");
 
+		const user = User.create({ name: 'Admin', login: 'admin', password: await bcrypt.hash('admin', 10) });
+		await User.save(user);
+
 		app.listen(config.PORT, () =>
   	console.log(`App is running on http://localhost:${config.PORT}`)
 		);
 	} else {
-		connection.connect();
+	  await connection.connect();
 	}
 }).catch((error: Error) => logger.error(`Error: ${error.message}`, error));
