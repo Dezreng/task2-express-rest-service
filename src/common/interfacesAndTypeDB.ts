6d6292d74a0d90660c5ac731a88d5f24280fdc74
Korshunov Egor <egor.dezreng@gmail.com>
2021-06-24 12:57:12 +0300

    feat: add new interface and new Error

--- src/common/interfacesAndTypeDB.ts
+++ src/common/interfacesAndTypeDB.ts
@@ -8,6 +8,8 @@ export type TaskDTO = Omit<Task, 'id'>;
 
 export type BoardDTO = Omit<Board, 'id'>;
 
+export type UserLogin = Omit<User, 'id' | 'name'>
+
 export interface errHandler extends Error {
 	name: string;
   message: string;
