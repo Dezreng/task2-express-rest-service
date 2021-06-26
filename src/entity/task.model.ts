f98873ae077af1262b270c8c9ac18dfcd82c996e
Korshunov Egor <egor.dezreng@gmail.com>
2021-06-26 13:39:22 +0300

    feat: add manytoone for userId and boardId

--- src/entity/task.model.ts
+++ src/entity/task.model.ts
@@ -1,4 +1,4 @@
-import { BaseEntity, Column, Entity, getConnection, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import Board from "./board.model";
 import User from "./user.model";
 
@@ -8,7 +8,7 @@ export default class Task extends BaseEntity {
 	@PrimaryGeneratedColumn("uuid")
 	id!: string;
 
-	@Column({ type: 'varchar', length: 50 })
+	@Column({ type: 'varchar', length: 100 })
 	title: string;
 
 	@Column('integer', { default: 0 })
@@ -17,34 +17,20 @@ export default class Task extends BaseEntity {
 	@Column({ type: 'varchar', length: 255, default: '' })
 	description!: string;
 
-	@Column({ nullable: true, default: null })
-	userId!: string;
+	@ManyToOne(() => User, user => user.id, {
+		nullable: true,
+		onDelete: 'SET NULL',
+	})
+	@JoinColumn({ name: 'userId' })
+	userId: string | null = null;
 
-	@Column()
+	@ManyToOne(() => Board, board => board.id, {
+		nullable: true,
+		onDelete: 'CASCADE',
+	})
+	@JoinColumn({ name: 'boardId' })
 	boardId: string;
 
 	@Column({ nullable: true, default: null })
-	columnId!: string;
-
-	static async fixUsersStructure(user: User){
-		if(user){
-			await getConnection()
-			.createQueryBuilder()
-    	.update(Task)
-    	.set({ userId: null })
-    	.where("userId = :userId", { userId: user.id })
-    	.execute();
-		}
-	}
-
-	static async fixBoardsStructure(board: Board){
-		if ( board ) {
-			await getConnection()
-				.createQueryBuilder()
-    		.delete()
-    		.from(Task)
-    		.where("boardId = :boardId", { boardId: board.id })
-    		.execute();
-		}
-	}
+	columnId: string | null;
 }
