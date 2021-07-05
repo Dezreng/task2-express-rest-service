ab5921b7342ecbb7df4350959088a98c4e04543a
Korshunov Egor <egor.dezreng@gmail.com>
2021-07-05 18:03:35 +0300

    feat: delete old express server

--- src/resources/login/login.service.ts
+++
@@ -1,6 +0,0 @@
-import { UserLogin } from '../../common/interfacesAndTypeDB';
-import loginRepo from './login.repository'
-
-const signIn = (reqBody: UserLogin) => loginRepo.signIn(reqBody);
-
-export default { signIn };
